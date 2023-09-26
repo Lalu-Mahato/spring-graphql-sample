c435a5488fd916a8372940b55a93a2cef2d02d33
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2023-09-26 11:14:57 +0530

    project setup with basic requirements

---
+++ src/main/java/com/example/springgraphqlsample/SpringGraphqlSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgraphqlsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlSampleApplication.class, args);
+	}
+
+}
